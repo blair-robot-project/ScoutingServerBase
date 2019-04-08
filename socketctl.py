e56fbf66605625c5985b764041afc4090ff4547b
Carter Wilson <carter47w@gmail.com>
2019-04-07 21:03:04 -0400

    Added color/number to mac names

--- socketctl.py
+++ socketctl.py
@@ -12,14 +12,13 @@ BACKLOG = 1
 SIZE = 4096
 
 # Associates MAC addresses of the kindles with their user friendly names
-MAC_DICT = {'78:E1:03:A4:F7:70': 'Poseidon',
-            '00:FC:8B:3B:42:46': 'Demeter',
-            '00:FC:8B:39:C1:09': 'Hestia',
-            '78:E1:03:A1:E2:F2': 'Hades',
-            '78:E1:03:A3:18:78': 'Hera',
-            '???': '???',
+MAC_DICT = {'00:FC:8B:3B:42:46': 'R1 Demeter',
+            '00:FC:8B:39:C1:09': 'R2 Hestia',
+            '78:E1:03:A3:18:78': 'R3 Hera',
+            '78:E1:03:A1:E2:F2': 'B1 Hades',
+            '78:E1:03:A4:F7:70': 'B2 Poseidon',
+            '00:FC:8B:3F:E4:EF': 'B3 Zeus',
             '00:FC:8B:3F:28:28': 'Backup 1',
-            '00:FC:8B:3F:E4:EF': 'Zeus',  # Secretly Backup 2
             '44:65:0D:E0:D6:3A': 'Strategy Tablet'}
 
 
