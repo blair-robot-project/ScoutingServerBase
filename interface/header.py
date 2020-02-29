93d4aa4930cf1cf0736a8a82c605b948499435f7
Carter Wilson <carter47w@gmail.com>
2020-02-28 23:08:29 -0500

    deleted old header

--- interface/header.py
+++ interface/header.py
@@ -45,28 +45,14 @@ def print_header(width=None):
     printing.printf(('{:^' + str(width) + '}').format('Bluetooth Scouting Server'), style=printing.TITLE)
     printing.printf()
     printing.printf(('{:^' + str(width) + '}').format('Runs with Python3 on Linux'), style=printing.INSTRUCTIONS)
-    printing.printf(('{:^' + str(width) + '}').format('Written by Carter Wilson, 2019'), style=printing.INSTRUCTIONS)
     printing.printf('-' * width + '\n', style=printing.INSTRUCTIONS)
 
-    printing.printf('Instructions for use:', style=printing.INSTRUCTIONS)
-    tw = TextWrapper(width=width)
-    printing.printf(tw.fill(
-        'This server is intended for use with the Team 449 scouting app (and strategy app) for android.') + '\n' +
-                    tw.fill('On each scouting tablet, please launch the scouting app, select your device name from the '
-                            'popup, and press connect. If there is no popup, press the Bluetooth icon in the top right '
-                            'corner of the app.') + '\n' +
-                    tw.fill('When connected, you should receive a popup in the app informing you that a connection was '
-                            'made, as well as output from this server with the name of the device.') + '\n' +
-                    tw.fill('To retrieve the data file for data analysis, plug in a USB flash drive, wait for output '
-                            'saying that it was mounted, the data was copied, and it was unmounted, then remove it.')
-                    + '\n' +
-                    tw.fill(
-                        'If the drive does not get detected/updated, it may be because there is no new data since you '
-                        'last updated it. If you would still like to update it (if, for example, you switched flash '
-                        'drives), just type d, data, u, or update into the server.') + '\n' +
-                    tw.fill(
-                        'To retrieve data for strategy, type "s" and hit enter, then enter the team numbers.') + '\n' +
-                    tw.fill('To quit the server, type "q" or "quit" and press enter, confirm that you want to '
-                            'quit, and wait for the server to close.'), style=printing.INSTRUCTIONS)
-    printing.printf()
-    printing.printf('-' * width + '\n\n', style=printing.UNDERLINE)
+    # printing.printf('Instructions for use:', style=printing.INSTRUCTIONS)
+    # tw = TextWrapper(width=width)
+    # printing.printf(tw.fill('Commands:') + '\n' +
+                    # tw.fill('q: quit') + '\n' +
+                    # tw.fill('d: request drive update (should be automatic)') + '\n' +
+                    # tw.fill('st: send team list to all connected devices') + '\n' +
+                    # tw.fill('ss: send match schedule to all connected devices'), style=printing.INSTRUCTIONS)
+    # printing.printf()
+    # printing.printf('-' * width + '\n\n', style=printing.UNDERLINE)
